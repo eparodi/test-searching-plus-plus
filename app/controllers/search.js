86d8a75efc0a913b534de49ba92df71971dfcca8
Eliseo Parodi Almaraz <eparodialmaraz@gmail.com>
2017-12-28 16:49:37 -0300

    Search implemented

--- app/controllers/search.js
+++ app/controllers/search.js
@@ -11,7 +11,14 @@ module.exports = function searchController(req, res, next) {
 
   const onDone = () => res.json(response);
   const onError = (err) => next(err);
-  const datasource = get();
+  var datasource = get();
+  datasource = datasource.filter( result => {
+      for (var key in result){
+          if (result[key].toString().includes(q)){
+              return true;
+          }
+      }
+  })
   datasource.take(100).toArray().subscribe((results) => {
     response.results = results
   }, onError, onDone);
